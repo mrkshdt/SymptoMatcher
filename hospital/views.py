2fec5f3f7ffe08c91bc063679e33ebab32efa033
Hackathon Teilnehmer 24 <hack024@rhrk.uni-kl.de>
2020-11-14 16:21:26 +0100

    Add Flask API Endpoint to get the most likely fachbereiche

--- hospital/views.py
+++ hospital/views.py
@@ -6,3 +6,21 @@ from. import data_processing
 def base():
 	dict_words = {}
 	return dict_words
+
+@app.route("/api/routing")
+def get_plot_data():
+    """
+    :query_parameter user_string: The user's description of his condition
+    """
+    start_total = time.time()
+    user_string = request.args.get('user_string')
+
+    # Get a ranking of appropriate fachbereiche
+    ranking, fb_matching_details = data_processing.string_to_fb(user_string)
+
+    end_total = time.time()
+    print("Total time until response: ", end_total - start_total)
+    return {
+        'ranking': ranking,
+        'matching_details': fb_matching_details
+    }
