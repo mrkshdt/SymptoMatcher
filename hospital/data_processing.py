98142401fed31d15037661ca36e4d4f568cc4898
Hackathon Teilnehmer 20 <hack020@rhrk.uni-kl.de>
2020-11-14 13:11:51 +0100

    data processing

---
+++ hospital/data_processing.py
@@ -0,0 +1,84 @@
+import re
+import string
+import nltk
+import json
+import fasttext
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import cosine_similarity
+MODEL_PATH = "resources/wiki.de.bin"
+
+# Keep the fasttext model as a global, cached variable 
+fastText_model = None
+german_stop_words = stopwords.words('german')
+
+def normalize(user_string):
+    
+    user_string = user_string.lower()
+    clean_rf = re.sub(r"""[-,.;@#?!&$]+\ *"""," ",user_string, flags=re.VERBOSE)
+    tokens = nltk.word_tokenize(clean_rf)
+    sol = [w for w in tokens if not w in german_stop_words]
+    
+    ### Extract specific german words? (Verbs, Adjectives,...)
+    #new = nlp(sol)
+    #print("Verbs:", [token.lemma_ for token in new if token.pos_ == "VERB"])
+    
+    return sol
+
+def similarity(word1, word2):
+    """
+    Computes the similarity between two words by their semantic meaning or their lexical similarity if one of the words is unknown.
+    """
+    # Load the fastText model if this is the first time the function is called
+    global fastText_model
+    fastText_model = fasttext.load_model(MODEL_PATH) if not fastText_model else fastText_model
+    embedding1 = fastText_model.get_word_vector(word1).reshape(1,-1)    # reshape needed to satisfy cosine function below
+    embedding2 = fastText_model.get_word_vector(word2).reshape(1,-1)
+    similarity = cosine_similarity(embedding1, embedding2)
+    return similarity
+
+def get_fb_ranking(fb_matched_terms):
+    """
+    Returns a fachbereich ranking according to how many tokens have been matched for each fachbereich.
+    """
+    # Count for each fachbereich how many tokens were detected as relevant and sort the fachbereiche by their count
+    ranking = sorted([(fb, len(fb_matched_terms[fb])) for fb in fb_matched_terms.keys()], key=lambda x: x[1], reverse=True)
+    return ranking
+
+def string_to_fb(user_string):
+    """
+    Checks for relevant terms of each fachbereich in the user_string
+    """
+    with open ('resources/terms.json', 'r') as f:
+        gazetteers = json.load(f, encoding='utf-8')
+
+    user_tokens = normalize(user_string)
+
+    # fb_matched_terms is a dictionary which maps every fachbereich to a score that represents how much the users description fits the fachbereich
+    fb_matched_terms = {}
+    # fb_matching_details maps for every fachbereich which user token has been detected as relevant by which fb_term 
+    fb_matching_details = {}
+    mentioned_bereiche = set()
+    thresh = 0.7
+    # Get the matches between tokens and relevant terms of each fachbereich
+    for bereich in gazetteers['Fachbereiche']:
+        fb_matched_terms[bereich['name']] = set()
+        fb_matching_details[bereich['name']] = []
+        for token in user_tokens:
+            # Check whether the name of the fachbereich has been mentioned in which case it should definitely be displayed as an option to the user
+            if similarity(token, bereich['name']) > thresh:
+                mentioned_bereich.add(bereich)
+            for alt_name in bereich['alt_names']:
+                if similarity(token, alt_name) > thresh:
+                    mentioned_bereich.add(bereich)
+
+            # Check whether the similarity of one of the tokens matches one of the words associated with the fachbereich
+            for fb_term in bereich['associated_terms']:
+                if similarity(token, fb_term)[0,0] > thresh:
+                    fb_matched_terms[bereich['name']].add(token)
+                    fb_matching_details[bereich['name']].append({'user_token':token, 'fb_term':fb_term, 'score':similarity(token, fb_term)[0,0]})
+
+    ranking = get_fb_ranking(fb_matched_terms)
+    print(ranking)
+    return ranking, fb_matching_details
+
+string_to_fb("Ich habe übertrieben harte Bauchschmerzen")
---
+++ hospital/resources/README.txt
@@ -0,0 +1,6 @@
+In order to use fasttexts word vectors, you need to download the following file:
+
+wiki.de.bin
+
+from the following link (download the german zip and extract the wiki.de.bin from it)
+https://fasttext.cc/docs/en/pretrained-vectors.html
